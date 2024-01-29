35c20e787952017589d5f62489dd585e17f39fbe
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-01-29 17:48:49 +0100

    feat: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent of Code 2021 🎄
+All puzzles of 2021 solved using Deno.js and JavaScript. 
+
+<img src="https://repository-images.githubusercontent.com/112706767/160be980-3b1a-11eb-9dbe-439a40adfa99" />
+
+This repository contains my solutions to the [Advent of Code challenge 2021](https://adventofcode.com/2021).
+Please play fair and use this repository for learning purposes only.
+
+## Commands
+
+This package offers a utility 'Taskfile.yml' which will scaffold a daily solution. 
+You can run `task add -- 1` to scaffold a solution for `day1`.
---
+++ tmp/solve.ts
@@ -0,0 +1,46 @@
+// @deno-types="npm:@types/lodash"
+import _ from "npm:lodash"
+import { read } from "../utils/Reader.ts"
+
+type Puzzle = string[][]
+
+const [task, sample] = read("day{DAY}")
+  .map((file) => file.split("\n").slice(0, -1))
+  .map((file) => file.map((l) => l))
+
+console.clear()
+console.log("🎄 Day {DAY}: YYY")
+
+const runPart1 = true
+const runPart2 = false
+const runBoth = false
+
+/// Part 1
+
+const solve1 = (data: Puzzle) => {
+  // TEMPLATE
+  for (const row of data) {
+    console.log(row)
+  }
+}
+
+const solve1Sample = runPart1 ? solve1(sample) : "skipped"
+const solve1Task = runPart1 && runBoth ? solve1(task) : "skipped"
+
+console.log("\nPart 1:")
+console.log("Sample:\t", solve1Sample)
+console.log("Task:\t", solve1Task)
+
+/// Part 2
+
+const solve2 = (data: Puzzle) => {
+  // TEMPLATE
+}
+
+const solve2Sample = runPart2 ? solve2(sample) : "skipped"
+const solve2Task = runPart2 && runBoth ? solve2(task) : "skipped"
+
+console.log("\nPart 2:")
+console.log("Sample:\t", solve2Sample)
+console.log("Task:\t", solve2Task)
+console.log()
