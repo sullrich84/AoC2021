8718cb64ff61bdeeb67969778ca0077c3214d2a3
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-01-30 15:36:14 +0100

    chore: wip

--- day16/solve.ts
+++ day16/solve.ts
@@ -20,69 +20,46 @@ const runBoth = false
 /// Part 1
 
 const solve1 = (data: Puzzle) => {
-  function parseLabel(binary: string): [number, string] {
-    const packets = _.chunk(binary, 5).map(([h, ...r]) => [h, r.join("")])
-    const length = packets.findIndex(([h]) => h === "0")
-    const literalPackets = packets.splice(0, length + 1)
-    const literalBytes = literalPackets.map(([_h, r]) => r).join("")
-    const literal = parseInt(literalBytes, 2)
-    return [literal, packets.flat().join("")]
-  }
+  const bin = data.split("")
+  let versionSum = 0
 
-  function parseOperator(binary: string): [string | number, string] {
-    const bin = binary.split("")
-    const type = bin.shift()
+  const readBits = (size: number) => bin.splice(0, size).join("")
+  const prevBits = (size?: number) => bin.join("").slice(0, size || bin.length)
+  const cutApdx = () => readBits(bin.length % 4)
+  const toInt = (value: string) => parseInt(value, 2)
 
-    if (type === "0") {
-      const lengthString = bin.splice(0, 15).join("")
-      const length = parseInt(lengthString, 2)
-      const payload = bin.splice(0, length).join("")
-      const rest = bin.join("")
-      return [payload, rest]
-    }
+  function parse(bin: string[]) {
+    const version = toInt(readBits(3))
+    const type = toInt(readBits(3))
+    versionSum += version
 
-    if (type !== "1") throw "Invalid type"
-    const lengthString = bin.splice(0, 11).join("")
-    const subPackets = parseInt(lengthString, 2)
-    const rest = bin.join("")
-    return [subPackets, rest]
-  }
-
-  function parse(binary: string, count = 0) {
-    if (/^0+$/.test(binary)) return count
-
-    console.log(binary)
-    const V = binary.substring(0, 3)
-    const T = binary.substring(3, 6)
-    const R = binary.substring(6)
-    const version = parseInt(V, 2)
-    const type = parseInt(T, 2)
-    count += version
-
-    if (type == 4) {
-      const [literal, rest] = parseLabel(R)
-      console.log('%c"%s"', "color: lime", literal)
-      count += parse(rest, count)
-    }
-
-    if (type != 4) {
-      const [operator, rest] = parseOperator(R)
-      if (_.isString(operator)) {
-        count += parse(operator, count)
-        count += parse(rest, count)
+    if (type === 4) {
+      let [val, end] = ["", false]
+      while (!end) {
+        end = readBits(1) === "0"
+        val += readBits(4)
+      }
+      console.log("LIT: %c%s", "color: lime", toInt(val))
+      console.log("APX: %c%s", "color: gray", cutApdx())
+    } else {
+      const lenType = readBits(1)
+      if (lenType == "0") {
+        const len = toInt(readBits(15))
+        const subOps = readBits(len)
+        versionSum += solve1(subOps)
+        console.log("OPS: %c%s", "color: purple", len)
       } else {
-        let [nOperator, nRest] = [operator, rest]
-        for (let o = 0; o < operator; o++) {
-          count += parse(nOperator, count, true)
-        }
-        count += parse(rest, count)
+        const len = toInt(readBits(11))
+        console.log("OPS: %c%s", "color: cyan", len)
       }
     }
-
-    return count
   }
 
-  return parse("11101110000000001101010000001100100000100011000001100000")
+  while (bin.length > 0) {
+    parse(bin)
+  }
+
+  return versionSum
 }
 
 const solve1Sample = runPart1 ? solve1(sample) : "skipped"
