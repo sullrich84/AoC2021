717c7c0ff2af54070eb7850b9450d4e2405b5e76
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-02-01 23:58:16 +0100

    chore: update day 19

--- day19/solve.ts
+++ day19/solve.ts
@@ -2,7 +2,9 @@
 import _ from "npm:lodash"
 import { read } from "../utils/Reader.ts"
 
-type Puzzle = string[][]
+type Puzzle = Scanner[]
+type Scanner = { scanner: number; beacons: Coord[] }
+type Coord = [x: number, y: number, z: number]
 
 const [task, sample] = read("day19")
   .map((file) => file.split("\n\n"))
@@ -26,14 +28,53 @@ const runBoth = false
 
 /// Part 1
 
+function hashBeacons(scanner: Scanner) {
+  let [xs, ys, zs] = [0, 0, 0]
+    
+  return [xs, ys, zs]
+}
+
+function hash([x, y, z]) {
+  const a = Math.abs(x - y)
+  const b = Math.abs(y - z)
+  const c = Math.abs(z - x)
+  return [a, b, c].sort((l, r) => l - r)
+}
+
+function rot([x, y, z]) {
+  return [
+    [x, y, z],
+    [x, z, -y],
+    [x, -y, -z],
+    [x, -z, y],
+    [-x, -y, z],
+    [-x, z, y],
+    [-x, y, -z],
+    [-x, -z, -y],
+    [y, z, x],
+    [y, x, -z],
+    [y, -z, -x],
+    [y, -x, z],
+    [-y, -z, x],
+    [-y, x, z],
+    [-y, z, -x],
+    [-y, -x, -z],
+    [z, x, y],
+    [z, y, -x],
+    [z, -x, -y],
+    [z, -y, x],
+    [-z, -x, y],
+    [-z, y, x],
+    [-z, x, -y],
+    [-z, -y, -x],
+  ]
+}
+
 const solve1 = (data: Puzzle) => {
-  for (let a = 0; a < data.length; a++) {
-    const scannerA = data[a]
-    for (let b = 0; b < data.length; b++) {
-      if (a == b) continue
-      const scannerB = data[b]
-      const intersections = _.intersection(scannerA, scannerB)
-      console.log(scannerA.scanner, scannerB.scanner, intersections)
+  for (const [aid, a] of data.entries()) {
+    for (const [bid, b] of data.entries()) {
+      if (aid == bid) continue
+      console.log("Comparing scanner", aid, "and", bid)
     }
   }
 
